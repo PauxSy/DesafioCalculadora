048c1cfc8d9cab86296b8fccf99a6e0967734f68
Paula Salgado <paula.salgado03@inacapmail.cl>
2022-08-08 22:42:49 -0400

    Actualizacion calculadoratest

--- src/main/java/cl/fsj/infoclub/calc/Calculadora.java
+++ src/main/java/cl/fsj/infoclub/calc/Calculadora.java
@@ -28,4 +28,36 @@ public class Calculadora {
 			return division;
 		
 	}
+
+	public Calculadora() {
+		super();
+	}
+
+	public Calculadora(double n1, double n2) {
+		super();
+		this.n1 = n1;
+		this.n2 = n2;
+	}
+
+	public double getN1() {
+		return n1;
+	}
+
+	public void setN1(double n1) {
+		this.n1 = n1;
+	}
+
+	public double getN2() {
+		return n2;
+	}
+
+	public void setN2(double n2) {
+		this.n2 = n2;
+	}
+
+	@Override
+	public String toString() {
+		return "Calculadora [n1=" + n1 + ", n2=" + n2 + "]";
+	}
+	
 }
--- src/test/java/cl/fsj/infoclub/calc/test/CalculadoraTest.java
+++ src/test/java/cl/fsj/infoclub/calc/test/CalculadoraTest.java
@@ -1,6 +1,27 @@
 package cl.fsj.infoclub.calc.test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.logging.Logger;
+
+import org.junit.jupiter.api.Test;
+
+import cl.fsj.infoclub.calc.Calculadora;
+import cl.infoclub.fsj.modelo.Persona;
+
 public class CalculadoraTest {
+	private static Logger log = Logger.getLogger("cl.fsj.infoclub.calc.test.calculadoratest");
+	private final Calculadora calcu = new Calculadora();
+	
+	@Test
+	public void testSumar(){
+		log.info("info test sumar");
+		Calculadora suma = new Calculadora(10d,5d);
+		String respServicio = sp.crearPersona(paulette);
+		assertEquals("Persona creada",respServicio);
+		
+	}
+	
 	
 	
 
